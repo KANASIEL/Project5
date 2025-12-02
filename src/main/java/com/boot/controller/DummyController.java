d69b60dcf92228351758e2fc73f81e25257a4314
KANASIEL <kanasieltg@gmail.com>
2025-12-02 10:40:26 +0900

    Project5 Init

---
+++ src/main/java/com/boot/controller/DummyController.java
@@ -0,0 +1,13 @@
+package com.boot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DummyController {
+
+    @GetMapping("/Dummy")
+    public String Dummy(){
+        return "Dummy";
+    }
+}
---
+++ src/main/java/com/boot/service/impl/DummyServiceImpl.java
@@ -0,0 +1,8 @@
+package com.boot.service.impl;
+
+import com.boot.service.DummyService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DummyServiceImpl implements DummyService {
+}
