46dcb598b848b141247589028d1e120bee4febb6
jseulmi <127377332+jseulmi@users.noreply.github.com>
2025-12-05 17:00:31 +0900

    Update NewsList.jsx

--- frontend/src/pages/NewsPage/NewsList.jsx
+++ frontend/src/pages/NewsPage/NewsList.jsx
@@ -127,12 +127,12 @@ function NewsList() {
             <path
               d="M11 19C15.4183 19 19 15.4183 19 11C19 6.58172 15.4183 3 11 3
                    C6.58172 3 3 6.58172 3 11C3 15.4183 6.58172 19 11 19Z"
-              stroke="#1976ff"
+              stroke="#1e40af"
               strokeWidth="2"
             />
             <path
               d="M21 21L16.65 16.65"
-              stroke="#1976ff"
+              stroke="#1e40af"
               strokeWidth="2"
             />
           </svg>
