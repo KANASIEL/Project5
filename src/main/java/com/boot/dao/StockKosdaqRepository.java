938c0d8f8fe58180a7adbc94c49f1d4c04721541
KANASIEL <kanasieltg@gmail.com>
2025-12-09 16:07:01 +0900

    권한오류 수정

--- src/main/java/com/boot/dao/StockKosdaqRepository.java
+++ src/main/java/com/boot/dao/StockKosdaqRepository.java
@@ -1,10 +1,11 @@
 package com.boot.dao;
 
 import com.boot.dto.StockKosdaqDTO;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface StockKosdaqRepository extends MongoRepository<StockKosdaqDTO, String> {
-    StockKosdaqDTO findByCode(String code);
+public interface StockKosdaqRepository extends MongoRepository<StockKosdaqDTO, Object> {
+    StockKosdaqDTO findByCode(@Param("param") String code);
 }
--- src/main/java/com/boot/dao/StockKospiRepository.java
+++ src/main/java/com/boot/dao/StockKospiRepository.java
@@ -1,10 +1,11 @@
 package com.boot.dao;
 
 import com.boot.dto.StockKospiDTO;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface StockKospiRepository extends MongoRepository<StockKospiDTO, String> {
-    StockKospiDTO findByCode(String code);
+public interface StockKospiRepository extends MongoRepository<StockKospiDTO, Object> {
+    StockKospiDTO findByCode(@Param("param")String code);
 }
