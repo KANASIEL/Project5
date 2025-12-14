41cbe71a3867eb101cd333afc0dda17da3da3e80
Jizeng <wldbswjd77750@gmail.com>
2025-12-14 23:51:35 +0900

    Update NewsList.jsx

--- frontend/src/pages/NewsPage/NewsList.jsx
+++ frontend/src/pages/NewsPage/NewsList.jsx
@@ -539,12 +539,16 @@ function NewsList() {
 					    ))}
 					  </div>
 					)}
-						<button className="icon-btn" onClick={handleSearch}>
-							<svg width="22" height="22" viewBox="0 0 24 24" fill="none">
-								<path d="M11 19C15.4183 19 19 15.4183 19 11C19 6.58172 15.4183 3 11 3C6.58172 3 3 6.58172 3 11C3 15.4183 6.58172 19 11 19Z" stroke="#1e40af" strokeWidth="2" />
-								<path d="M21 21L16.65 16.65" stroke="#1e40af" strokeWidth="2" />
-							</svg>
-						</button>
+						<button
+					  type="button"
+					  className="icon-btn"
+					  onMouseDown={() => handleSearch()}
+					>
+					  <svg width="22" height="22" viewBox="0 0 24 24" fill="none">
+					    <path d="M11 19C15.4183 19 19 15.4183 19 11C19 6.58172 15.4183 3 11 3C6.58172 3 3 6.58172 3 11C3 15.4183 6.58172 19 11 19Z" stroke="#1e40af" strokeWidth="2" />
+					    <path d="M21 21L16.65 16.65" stroke="#1e40af" strokeWidth="2" />
+					  </svg>
+					</button>
 					</div>
 					
 					{/* 🔥 인기검색어 표시 */}
@@ -797,3 +801,4 @@ function NewsList() {
 }
 
 export default NewsList;
+
